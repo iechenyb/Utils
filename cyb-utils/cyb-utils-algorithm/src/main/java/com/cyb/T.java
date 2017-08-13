6483c14c84dd3df0921e70c173529cd44a6cd638
iechenyb <zzuchenyb@sina.com>
2017-08-13 19:55:55 +0800

    新增工具工程

---
+++ cyb-utils/cyb-utils-algorithm/src/main/java/com/cyb/T.java
@@ -0,0 +1,14 @@
+package com.cyb;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+/**
+ *作者 : iechenyb<br>
+ *类描述: 说点啥<br>
+ *创建时间: 2017年8月13日
+ */
+public class T {
+	Log log = LogFactory.getLog(T.class);
+	public static void main(String[] args) {
+		System.out.println("dd");
+	}
+}
